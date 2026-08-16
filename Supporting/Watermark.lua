68e98d1e83de67bb1d342faec7ef2c765dee9236
onlyonestands <imcomingforyou6959@gmail.com>
2026-08-15 20:48:24 -0700

    Update Watermark.lua

--- Supporting/Watermark.lua
+++ Supporting/Watermark.lua
@@ -1,30 +1,22 @@
-_G.WatermarkConnection = nil
-_G.WatermarkRunning = true
-
 Library:SetWatermarkVisibility(true)
 
-_G.GetPing = (function() return math.floor(game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValue()) end)
-_G.CanDoPing = pcall(function() return _G.GetPing() end)
+GetPing = (function() return math.floor(game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValue()) end)
+CanDoPing = pcall(function() return GetPing() end)
 
-local function StartWatermark()
-    _G.WatermarkRunning = true
-    
-    _G.WatermarkConnection = game:GetService("RunService").RenderStepped:Connect(function()
-        if not _G.WatermarkRunning then return end
-        
-        pcall(function()
-            if _G.CanDoPing then
-                local ping = _G.GetPing()
-                Library:SetWatermark("Rawr.xyz <3 | " .. ping .. " ms")
-            else
-                Library:SetWatermark("Rawr.xyz <3")
-            end
-        end)
-    end)
-end
-
-StartWatermark()
+WatermarkConnection = game:GetService("RunService").RenderStepped:Connect(function()
+    if CanDoPing then
+        Library:SetWatermark(("Rawr.xyz <3 | %d ms"):format(GetPing()))
+    else
+        Library:SetWatermark("Rawr.xyz <3")
+    end
+end)
 
 Library:OnUnload(function()
-    print("!")
+    if WatermarkConnection then
+        WatermarkConnection:Disconnect()
+        WatermarkConnection = nil
+    end
+
+    print("Unloaded!")
+    Library.Unloaded = true
 end)
