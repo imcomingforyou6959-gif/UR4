abc4f64210c2cb44be6d6a58317da83ab820a49c
onlyonestands <imcomingforyou6959@gmail.com>
2026-09-23 18:58:52 -0700

    Update sizes for Collapsed and Expanded states
    Adjusted the height of CollapsedSize and ExpandedSize.

--- addons/Extra.lua
+++ addons/Extra.lua
@@ -3735,8 +3735,8 @@ function Library:CreateSpotifyPlayer()
     local TokenConfig = DecodeTokenConfig(ReadToken())
     local Token = TokenConfig.AccessToken
 
-    local CollapsedSize = UDim2.new(0, 248, 0, 88)
-    local ExpandedSize  = UDim2.new(0, 540, 0, 340)
+    local CollapsedSize = UDim2.new(0, 248, 0, 90)
+    local ExpandedSize  = UDim2.new(0, 540, 0, 280)
     local ResultButtons = {}
     local SearchResults = {}
     local SearchTrackResults = {}
